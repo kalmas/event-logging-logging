6e27d77e99693d464dfad1077f392566f766e7bc
kalmas <kylealmas@gmail.com>
2013-02-02 23:31:42 -0500

    made it a bookmarklet, made the tooltip a little snazzier

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-event-logging-logging
-=====================
+Event Logging Logger
+====================
 
-Event Logging^2
+This bookmarklet gives you an easy way to look at click events. (Event Logging^2, if you will :-)
+
+Usage
+-----
+1. Drag this link into your bookmarks:
+[Event Logging Logger](javascript:(function(){js=document.createElement('SCRIPT');js.type='text/javascript';js.src='http://localhost/cruzers/event-logging-logging/ell.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(js);css=document.createElement('LINK');css.rel='stylesheet';css.type='text/css';css.href='http://localhost/cruzers/event-logging-logging/ell.css?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(css);})();)
+2. Navigate to page you want to inspect and click the bookmark
--- ell.js
+++ ell.js
@@ -1,38 +1,93 @@
 (function(){
-  var $ = window.jQuery;
-	
+  	var $ = window.jQuery;
+	// methods	
+
 	function init(){
-		$('[onmousedown],[onclick]').each(function(){makeItPop($(this))});
+		$('body').append('<div id="ell-info">Hello</div>');
+		$('[onmousedown],[onclick]').each(function(){highlight($(this), 'inline')});
+
 	}
 	
-	function makeItPop(target){
-		target.css("border", "2px solid red");
-		target.mouseenter(function(){
-			$(this).css("background-color","grey");
+	function showInfo(event){
+		target = $(event.target);
+		// Image elements are a bit wacky, so move the target up the dom
+		if(target.is('img')){
+			target = target.parent();
+		}
+		target.css('background-color', 'grey');
+		$('#ell-info').css("display", "block");
+		$('#ell-info').empty();
+		var stmts = JSON.parse(target.data('ell-statements'));
+		for(var i = 0; i < stmts.length; i++){
+			$('#ell-info').append('<li>' + stmts[i] + '</li>');
+		}
+		target.bind('mousemove', function(e){
+		    $('#ell-info').css({
+		       left:  e.pageX - 20,
+		       top:   e.pageY + 20
+		    });
 		});
-		target.mouseleave(function(){
-			$(this).css("background-color","");
-		});
-		if(typeof target.attr("onmousedown") == "undefined"){
-			var scriptText = target.attr("onclick");
-		} else if(typeof target.attr("onclick") == "undefined"){
-			var scriptText = target.attr("onmousedown");
-		} else {
-			var scriptText = target.attr("onclick") + "|" + target.attr("onmousedown");
-		}
-		
-		target.attr("title", scriptText);
-		
-		if(scriptText.match(/linkTrack/i)){
-			target.css("border", "2px solid pink");
-		}
-		if(scriptText.match(/scLogger/i)){
-			target.css("border", "2px solid yellow");
-		}
-		if(scriptText.match(/common_log_event/i)){
-			target.css("border", "2px solid orange");
-		}
+	}
+	
+	function hideInfo(event){
+		target = $(event.target);
+		target.css('background-color', '');
+		$('#ell-info').css("display", "none");
+		target.unbind('mousemove');
 	}
 
+	function highlight(target, bindingType){
+		target.css("border", "2px solid grey");
+
+		if(bindingType == 'inline'){
+			var scriptTexts = [];
+			if(typeof target.attr("onmousedown") != "undefined"){
+				scriptTexts.push(target.attr("onmousedown"));
+			} 			
+			if(typeof target.attr("onclick") != "undefined"){
+				scriptTexts.push(target.attr("onclick"));
+			}
+			
+			var statements = [];
+			for(var i = 0; i < scriptTexts.length; i++){
+				var str = scriptTexts[i].replace(/^javascript:/, '');
+				str = str.replace(/;$/, '');
+				str = $.trim(str);
+				statements = statements.concat(str.split(";"));
+			}
+			
+		} else {
+			// TODO: Grab functions for unobtrusivly bound events
+			var statements = [];
+		}
+		
+		target.data("ell-statements", JSON.stringify(statements));
+		target.mouseenter(showInfo);
+		target.mouseleave(hideInfo);
+
+		if(statements.length == 1){
+			if(statements[0].match(/^common_log_event/)){
+				target.css("border", "2px solid pink");
+			} else if(statements[0].match(/^linkTrack/)){
+				target.css("border", "2px solid yellow");
+			} else if(statements[0].match(/^scLogger/)){
+				target.css("border", "2px solid orange");
+			}
+		} else {
+			target.css("border", "2px solid red");
+		}
+		
+//		if(scriptText.match(/linkTrack/i)){
+//			target.css("border", "2px solid pink");
+//		}
+//		if(scriptText.match(/scLogger/i)){
+//			target.css("border", "2px solid yellow");
+//		}
+//		if(scriptText.match(/common_log_event/i)){
+//			target.css("border", "2px solid orange");
+//		}
+	}
+	
 	$(document).ready(init);
-})
+
+}());
