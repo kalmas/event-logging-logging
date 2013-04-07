2b2d74b0979fd3b5419ba3ec1600ce0f3889781a
kalmas <kylealmas@gmail.com>
2013-04-07 10:44:29 -0400

    reorganized

--- runner/bookmarklet.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){js=document.createElement('SCRIPT');js.type='text/javascript';js.src='http://localhost/cruzers/event-logging-logging/ell.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(js);})();
