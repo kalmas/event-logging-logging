3b79f960dd7b5fcec95fcca9b37a857cae569c70
kalmas <kylealmas@gmail.com>
2013-04-07 11:53:22 -0300

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){js=document.createElement('SCRIPT');js.type='text/javascript';js.src='http://localhost/cruzers/event-logging-logging/ell.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(js);})();
+javascript:(function(){js=document.createElement('SCRIPT');js.type='text/javascript';js.src='https://raw.github.com/kalmas/event-logging-logging/master/ell.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(js);})();
